2b93a8366027ee03f18f866a03be26fa1c5d544c
pankaj dasbhaya <p9951542@gmail.com>
2026-03-23 00:38:04 +0530

    first commit

---
+++ 01_first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// code explained:
+// ye code ek simple C program hai jo "Hello, World!" print karta hai.
+// isme humne stdio.h header file include ki hai jo input/output functions provide karti hai.
+// main() function program ka entry point hai, jahan se execution start hota hai.
+// printf() function ka use karke humne "Hello, World!" message ko console par print kiya hai.
+// return 0; statement indicate karta hai ki program successfully execute hua hai.  

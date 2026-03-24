1b43586d89209c090809a2a3b7ce26f38414498a
pankaj dasbhaya <p9951542@gmail.com>
2026-03-24 15:44:42 +0530

    add

--- 02_variabes.c
+++ 02_variabes.c
@@ -4,13 +4,13 @@ int main()
 {
 
     // int a=5;  aasa bhi likh sakte hai jisme variable declare karte waqt hi usko value assign kar dete hai
-    int a ;  
-    a=5;
-    printf("%d\n",a);
+    int a;
+    a = 5;
+    printf("%d\n", a);
     return 0;
 }
 
-// yahan par ouput aayega 5 
+// yahan par ouput aayega 5
 
 // code explanation:
 // int a;  yaha humne ek variable a declare kiya hai jiska type int(integer) hai
@@ -19,11 +19,13 @@ int main()
 // %d format specifier hai jo integer value print karta hai
 // \n new line character hai jo print ke baad new line me chala jata hai
 
-
 // RULES FOR NAMING VARIABLES IN C:
 // 1. Variable name can only contain letters, digits and underscores
-// 2. Variable name must start with a letter or an underscore  
+// 2. Variable name must start with a letter or an underscore
 // 3. Variable name cannot be a keyword
 // 4. Variable name cannot contain spaces
 // 5. Variable name is case sensitive
 // 6. Variable name should be meaningful and descriptive
+
+// 32 keywords c language ke reserved words hote hai jinko hum variable name ke liye use nahi kar sakte hai
+

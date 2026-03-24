e546365a504c9a62dc6bc08eaf08e1b336bbe17c
pankaj dasbhaya <p9951542@gmail.com>
2026-03-24 15:27:56 +0530

    add variables

---
+++ 02_variabes.c
@@ -0,0 +1,18 @@
+// variable ek container hota hai jisme hum data store karte hai
+#include <stdio.h>
+int main()
+{
+
+    // int a=5;  aasa bhi likh sakte hai jisme variable declare karte waqt hi usko value assign kar dete hai
+    int a ;  
+    a=5;
+    printf("%d\n",a);
+    return 0;
+}
+
+// code explanation:
+// int a;  yaha humne ek variable a declare kiya hai jiska type int(integer) hai
+// a=5;  yaha humne variable a ko 5 value assign ki hai
+// printf("%d\n",a); yaha humne variable a ki value print ki hai
+// %d format specifier hai jo integer value print karta hai
+// \n new line character hai jo print ke baad new line me chala jata hai

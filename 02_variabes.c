a8316ab5ca1affd6f3ac8b85f28d61c8bd8159c1
pankaj dasbhaya <p9951542@gmail.com>
2026-03-24 15:30:35 +0530

    ADD

--- 02_variabes.c
+++ 02_variabes.c
@@ -18,3 +18,12 @@ int main()
 // printf("%d\n",a); yaha humne variable a ki value print ki hai
 // %d format specifier hai jo integer value print karta hai
 // \n new line character hai jo print ke baad new line me chala jata hai
+
+
+// RULES FOR NAMING VARIABLES IN C:
+// 1. Variable name can only contain letters, digits and underscores
+// 2. Variable name must start with a letter or an underscore  
+// 3. Variable name cannot be a keyword
+// 4. Variable name cannot contain spaces
+// 5. Variable name is case sensitive
+// 6. Variable name should be meaningful and descriptive
